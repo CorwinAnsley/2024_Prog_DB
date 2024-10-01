77a0cc766066cadcd9dfad25faca04b8576bbf56
CorwinAnsley <coreyansley@gmail.com>
2024-10-01 13:18:20 +0100

    completed lab 1

--- Lab1.py
+++ Lab1.py
@@ -1,7 +1,7 @@
 SEQ_1 = 'ACTGATCGATTACGTATAGTATTTGCTATCATACATATATATCGATGCGTTCAT'
 SEQ_2 = 'ATGGATCGATCGATCGACTGACTAGTCATAGCTATGCATGTAGCTACTCGATCGATCGATCGATCGATCGATCGATCGATCGATCATGCTATCATCGATCGATATCGATGCATCGACTACTAA'
 TEST = 'GCA'
-EXONS = [[0,62],[92,'']]
+EXONS = [[0,62],[92,None]]
 COMP_DICT = {'A': 't',
              'T': 'a',
              'G': 'c',
@@ -25,9 +25,26 @@ def get_complement(seq):
     
     return seq.upper()
 
-def print_exon_info(seq):
-    seq = str(seq).lower()
+def get_exon_info(seq, exon_positions):
+    comp_seq = str(seq).lower()
+
+    exons = []
+    for pos in exon_positions:
+        exon = seq[pos[0]:pos[1]]
+        comp_seq = comp_seq[0:pos[0]] + exon + comp_seq[pos[1]:]
+        exons.append(exon)
+
+    mRNA = ''.join(exons).replace('T','U')
+    coding_pc = len(mRNA)*100 / len(seq)
+    return mRNA, coding_pc, comp_seq
+    
 
 
 #print(get_complement(SEQ_1))
-print(SEQ_2)
+#print(SEQ_2)
+
+mRNA, coding_pc, comp_seq = get_exon_info(SEQ_2, EXONS)
+print(f'mRNA: {mRNA}')
+print(f'%coding: {coding_pc}')
+print(f'full sequence: {comp_seq}')
+
