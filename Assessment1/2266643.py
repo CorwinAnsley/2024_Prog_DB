4a27aef1146a8d2642b2c9aea88fef0f23211505
CorwinAnsley <coreyansley@gmail.com>
2024-11-21 12:37:56 +0000

    created function to get junctions from cigar string

--- Assessment1/2266643.py
+++ Assessment1/2266643.py
@@ -5,6 +5,7 @@ import argparse
 ### there was not too few
 
 TEST_SAM = 'Toxo_chr8_subset.sam'
+TEST_CIGAR = '1S10M7887N89M'
 
 #  Dict of which colum in the sam file the relevant data is stored
 SAM_COLUMNS = {'RNAME':2, 'POS':3, 'CIGAR':5, 'NH:i:x':-1}
@@ -29,8 +30,39 @@ def parse_sam(filepath):
                 read_list.append(read_dict)
         return read_list
 
+# returns the junctions form cigar string and position
+def get_junctions(cigar, pos):
+    junction_list = []
+    
+    # This placeholder string holds section of the cigar string as we iterate over it
+    cigar_section = ''
+
+    for c in cigar:
+        # add digits to the placeholder
+        if c.isdigit():
+            cigar_section += str(c)
+        # if there's an 'M' we have a match
+        elif c == 'M':
+            # adjust the position and erase the placeholder string
+            pos += int(cigar_section)
+            cigar_section = ''
+            # if there's an 'N' we have a skipped region
+        elif c == 'N':
+            # If there's a skipped region we have a junction, using the position it is defined as string START:END 
+            # and added to the list of junctions
+            junction = f'{pos}:{pos + int(cigar_section)}'
+            junction_list.append(junction)
+            cigar_section = ''
+        # If there's an 'S', 'I' or anything else it is ignored along with the preceding digits
+        else:
+            cigar_section = ''
+        
+        print(junction_list)
+
+
+
 # Returns a dictionary; every key is a junction and the values are the number of reads matching the associated junction
-def get_junctions(read_list):
+def create_junctions_dict(read_list):
     junction_dict = {}
 
     for read in read_list:
@@ -39,6 +71,8 @@ def get_junctions(read_list):
 
 if __name__ == '__main__':
     read_list =  parse_sam(TEST_SAM)
-    for i in range(25):
-        print(read_list[i])
+    #for i in range(25):
+    #    print(read_list[i])
+
+    get_junctions(TEST_CIGAR, 0)
                 
