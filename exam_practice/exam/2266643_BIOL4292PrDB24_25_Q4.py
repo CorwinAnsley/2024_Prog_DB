e012aee6e38fba3bf8b596544110bbb27051e64c
CorwinAnsley <2266643A@student.gla.ac.uk>
2024-12-12 21:49:05 +0000

    finished exam

--- exam_practice/exam/2266643_BIOL4292PrDB24_25_Q4.py
+++ exam_practice/exam/2266643_BIOL4292PrDB24_25_Q4.py
@@ -3,6 +3,8 @@ VALID_BASES = ['A','T','G','C']
 
 # Takes no arguments, prompts the user for a sequence and makes sure it is valid
 # If not the user is prompted again until they provide a valid sequence
+# Returns a string of the user provided sequence
+# Should not throw any errors or exit until valid sequence is provided
 def get_user_seq():
     while True:
         try:
@@ -16,6 +18,7 @@ def get_user_seq():
                 if base not in VALID_BASES:
                     raise Exception('Invalid base in sequence')
 
+            # If valid proceed
             break
 
         except Exception as err:
@@ -23,8 +26,11 @@ def get_user_seq():
     
     return user_seq
 
-# Takes no arguments, prompts the user for a value of k and checks that it is valid
-def get_k():
+# Takes sequence as argument, prompts the user for a value of k and checks that it is valid
+# If not prompts the user again until valid k is provided
+# Returns an integer of the user provided k
+# Should not throw any errors or exit until valid k is provided
+def get_k(seq):
     while True:
         try:
             user_k = input('Please input k to find k-mers: ')
@@ -36,13 +42,22 @@ def get_k():
                 user_k = int(user_k)
             except:
                 raise Exception('Unable to convert input to integer')
+            
+            if user_k == 0:
+                raise Exception('Cannot compute 0-mer')
+            
+            if len(seq) < user_k:
+                raise Exception('Provided k is greater than the length of the sequence')
+            
+            # If valid proceed
+            break
         except Exception as err:
             print(f'{err}, try again')
     
     return user_k
 
 # With a given k and sequence as arguments get all k-mers and store in a dict
-# Returns the dict with kmers
+# Returns the dict with kmers as keys and how many times each kmer appears as values
 def get_kmers(seq, k):
     kmer_dict = {}
     for i in range(len(seq)):
@@ -55,11 +70,16 @@ def get_kmers(seq, k):
     
     return kmer_dict
 
-# Takes k-mer dict, sequence, and k as arguments and outputs a report for the user
+# Takes k-mer dict, sequence, and k as arguments and outputs a report of all k-mers and their frequency for the user
+# Returns None
 def print_kmers(kmer_dict, seq, k):
     print(f'Reporting {k}-mers for sequence: {seq}\n')
 
+    for kmer in kmer_dict:
+        print(f'{kmer}: {kmer_dict[kmer]}\n')
+
 if __name__ == '__main__':
     seq = get_user_seq()
-    k = get_k()
+    k = get_k(seq)
     kmer_dict = get_kmers(seq, k)
+    print_kmers(kmer_dict, seq, k)
